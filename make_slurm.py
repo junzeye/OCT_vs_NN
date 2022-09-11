0204fe4bd262a1bc67142411284a31bde30f8c38
Tony Ye <junzeye@gmail.com>
2022-09-11 19:28:34 -0400

    changed slurm scripts

--- make_slurm.py
+++ make_slurm.py
@@ -6,8 +6,6 @@ depth = [4,6,8]
 seeds = [37, 42, 53]
 hidden_dims = [20, 40, 60, 80, 100, 120]
 
-print_flag = False
-
 # create the folder to store all the slurm .out files
 dirpath = 'synthetic_tests/CTG_width/out'
 os.makedirs(dirpath, exist_ok = True)
@@ -22,13 +20,12 @@ for a, d, s in product(alpha, depth, seeds):
         '#!/bin/bash\n'
         f'#SBATCH --job-name=oct_a{a}_d{d}_s{s}_array\n'
         '#SBATCH --nodes=1                # node count\n'
-        '#SBATCH --ntasks=1               # total number of tasks across all nodes\n'
+        '#SBATCH --ntasks=6               # total number of tasks across all nodes\n'
         '#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)\n'
         '#SBATCH --mem-per-cpu=16G         # memory per cpu-core (4G is default)\n\n'
         '#SBATCH --array=20,40,60,80,100,120%1 # use hidden_dim as task ID\n'
         f'#SBATCH --output=../../synthetic_tests/CTG_width/out/oct_a{a}_d{d}_s{s}_%a.out    # Standard output and error log\n'
         '#SBATCH --time=3:59:00          # total run time limit (HH:MM:SS)\n'
-        '#SBATCH --mail-type=begin        # send email when job begins, but not for end.\n'
         '#SBATCH --mail-type=fail         # send email if job fails\n'
         '#SBATCH --mail-user=junzey@princeton.edu\n\n'
 
