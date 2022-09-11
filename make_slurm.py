32f17c7fc690b19f7ffad98b31f50d51134e1938
Tony Ye <junzeye@gmail.com>
2022-09-11 19:36:46 -0400

    slurm

--- make_slurm.py
+++ make_slurm.py
@@ -14,17 +14,16 @@ os.makedirs(dirpath, exist_ok = True)
 dirpath = 'slurm_scripts/vary_width'
 os.makedirs(dirpath, exist_ok = True)
 
-for a, d, s in product(alpha, depth, seeds):
+for a, d, s, hidden_dim in product(alpha, depth, seeds, hidden_dims):
 
     slurm_msg = (
         '#!/bin/bash\n'
-        f'#SBATCH --job-name=oct_a{a}_d{d}_s{s}_array\n'
+        f'#SBATCH --job-name=oct_dim{hidden_dim}_a{a}_d{d}_s{s}\n'
         '#SBATCH --nodes=1                # node count\n'
-        '#SBATCH --ntasks=6               # total number of tasks across all nodes\n'
+        '#SBATCH --ntasks=1               # total number of tasks across all nodes\n'
         '#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)\n'
         '#SBATCH --mem-per-cpu=16G         # memory per cpu-core (4G is default)\n\n'
-        '#SBATCH --array=20,40,60,80,100,120%1 # use hidden_dim as task ID\n'
-        f'#SBATCH --output=../../synthetic_tests/CTG_width/out/oct_a{a}_d{d}_s{s}_%a.out    # Standard output and error log\n'
+        f'#SBATCH --output=./synthetic_tests/CTG_width/out/oct_dim{hidden_dim}_a{a}_d{d}_s{s}.out    # Standard output and error log\n'
         '#SBATCH --time=3:59:00          # total run time limit (HH:MM:SS)\n'
         '#SBATCH --mail-type=fail         # send email if job fails\n'
         '#SBATCH --mail-user=junzey@princeton.edu\n\n'
@@ -33,12 +32,11 @@ for a, d, s in product(alpha, depth, seeds):
         'module load anaconda3/2022.5 gurobi/9.5.1\n'
         'conda activate thesis\n\n'
 
-        'cd ../../\n'
         'python experiment_instance.py '
-        f'--a {a} --d {d} --s {s} --hidden_dim SLURM_ARRAY_TASK_ID'
+        f'--a {a} --d {d} --s {s} --hidden_dim {hidden_dim}'
     )
 
-    script_name = dirpath + '/' + f'oct_a_{a}_d_{d}_s_{s}_array.slurm'
+    script_name = dirpath + '/' + f'oct_dim_{hidden_dim}_a_{a}_d_{d}_s_{s}.slurm'
 
     with open(script_name, 'w') as f:
         f.write(slurm_msg)
