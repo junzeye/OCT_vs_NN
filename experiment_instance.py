aa1b56f28d77a70b3c1ffdb9045358e85e8b0960
Tony Ye <junzeye@gmail.com>
2022-09-12 22:01:46 -0400

    updated slurm scripts. god bless

--- experiment_instance.py
+++ experiment_instance.py
@@ -18,7 +18,7 @@ parser.add_argument('--hidden_dim', type=int, required=True)
 # the following three are optional arguments
 parser.add_argument('--min_samples_split', type=int, default=2, 
                     help='leaf node minimum value')
-parser.add_argument('--timelimit', type=int, default=13800) # Gurobi timeout limit (3.5hr)
+parser.add_argument('--timelimit', type=int, default=13800) # Gurobi timeout limit (3.8hr)
 parser.add_argument('--data', type=str, default='CTG_width') # name of the dataset
 
 args = parser.parse_args()
@@ -58,24 +58,27 @@ row = res_oct[(res_oct['instance'] == data) & (res_oct['depth'] == d) &
             (res_oct['alpha'] == a) & (res_oct['seed'] == s) & 
             (res_oct['gap'] <= MIPGAP_TOLERANCE) & (res_oct['hidden_dim'] == hidden_dim)]
 if len(row): # if the specific oct is already trained and optimized up to a degree, we directly print the previously recorded results
-    print(data, 'hidden_dim = {}, oct-d{}-a{}-l{}'.format(hidden_dim, d, a, min_samples_split),
-        'train acc:', row['train_acc'].values[0], 'val acc:', row['val_acc'].values[0],
-        'test acc:', row['test_acc'].values[0],
-        'gap:', row['gap'].values[0])
+    msg = {'instance':data, 'hidden_dim':hidden_dim, 'depth':d, 'min_leaf':min_samples_split, 'alpha':a, 'seed':s, 'train_acc':row['train_acc'].values[0], 
+    'val_acc':row['val_acc'].values[0], 'test_acc':row['test_acc'].values[0], 'train_time':row['train_acc'].values[0], 'gap':row['gap'].values[0]}
+    print(json.dumps(msg, indent = 4))
+
 else: # tree has not been trained, so train
     octree = optimalDecisionTreeClassifier(max_depth=d, min_samples_split=min_samples_split, alpha=a, warmstart=True,
                                                 timelimit=timelimit, output=True)
-    tick = time.time()
-    octree.fit(x_train, y_train)
-    tock = time.time()
-    train_time = tock - tick
+    filepath = f'./synthetic_tests/CTG_width/gurobi_logs/oct_dim_{hidden_dim}_a_{a}_d_{d}_l{min_samples_split}_s_{s}.txt'
     
+    orig_stdout = sys.stdout
+    with open(filepath, 'w') as f: # redirect the Gurobi logs to a different file
+        sys.stdout = f
+        tick = time.time()
+        octree.fit(x_train, y_train)
+        tock = time.time()
+        train_time = tock - tick
+    sys.stdout = orig_stdout
+
     train_acc = accuracy_score(y_train, octree.predict(x_train))
     val_acc = accuracy_score(y_val, octree.predict(x_val))
     test_acc = accuracy_score(y_test, octree.predict(x_test))
-    row = {'instance':data, 'hidden_dim':hidden_dim, 'depth':d, 'alpha':a, 'seed':s, 'train_acc':train_acc, 'val_acc':val_acc,
+    row = {'instance':data, 'hidden_dim':hidden_dim, 'depth':d, 'min_leaf': min_samples_split, 'alpha':a, 'seed':s, 'train_acc':train_acc, 'val_acc':val_acc,
         'test_acc':test_acc, 'train_time':train_time, 'gap':octree.optgap}
-    res_oct = pd.concat([res_oct, pd.DataFrame.from_records([row])], ignore_index=True)
-    res_oct.to_csv(CSV_FILEPATH, index=False)
-    print()
     print(json.dumps(row, indent = 4))
--- make_slurm.py
+++ make_slurm.py
@@ -4,27 +4,28 @@ from itertools import product
 alpha = [0.001, 0.01, 0.1]
 depth = [4,6,8]
 seeds = [37, 42, 53]
+min_samples_splits = [2]
 hidden_dims = [20, 40, 60, 80, 100, 120]
 
 # create the folder to store all the slurm .out files
-dirpath = 'synthetic_tests/CTG_width/out'
+dirpath = 'synthetic_tests/CTG_width'
 os.makedirs(dirpath, exist_ok = True)
 
 # create the folder to store all the slurm scripts
 dirpath = 'slurm_scripts/vary_width'
 os.makedirs(dirpath, exist_ok = True)
 
-for a, d, s, hidden_dim in product(alpha, depth, seeds, hidden_dims):
+for a, d, s, hidden_dim, l in product(alpha, depth, seeds, hidden_dims, min_samples_splits):
 
     slurm_msg = (
         '#!/bin/bash\n'
-        f'#SBATCH --job-name=oct_dim{hidden_dim}_a{a}_d{d}_s{s}\n'
+        f'#SBATCH --job-name=oct_dim{hidden_dim}_a{a}_d{d}_l{l}_s{s}\n'
         '#SBATCH --nodes=1                # node count\n'
         '#SBATCH --ntasks=1               # total number of tasks across all nodes\n'
         '#SBATCH --cpus-per-task=1        # cpu-cores per task (>1 if multi-threaded tasks)\n'
         '#SBATCH --mem-per-cpu=18G         # memory per cpu-core (4G is default)\n\n'
-        f'#SBATCH --output=./synthetic_tests/CTG_width/out/oct_dim{hidden_dim}_a{a}_d{d}_s{s}.out    # Standard output and error log\n'
-        '#SBATCH --time=3:59:00          # total run time limit (HH:MM:SS)\n'
+        f'#SBATCH --output=./synthetic_tests/CTG_width/oct_dim{hidden_dim}_a{a}_d{d}_l{l}_s{s}.out    # Standard output and error log\n'
+        '#SBATCH --time=00:01:00          # total run time limit (HH:MM:SS)\n'
         '#SBATCH --mail-type=fail         # send email if job fails\n'
         '#SBATCH --mail-user=junzey@princeton.edu\n\n'
 
@@ -33,10 +34,10 @@ for a, d, s, hidden_dim in product(alpha, depth, seeds, hidden_dims):
         'conda activate thesis\n\n'
 
         'python experiment_instance.py '
-        f'--a {a} --d {d} --s {s} --hidden_dim {hidden_dim}'
+        f'--a {a} --d {d} --s {s} --hidden_dim {hidden_dim} --min_samples_split {l}'
     )
 
-    script_name = dirpath + '/' + f'oct_dim_{hidden_dim}_a_{a}_d_{d}_s_{s}.slurm'
+    script_name = dirpath + '/' + f'oct_dim_{hidden_dim}_a_{a}_d_{d}_l{l}_s_{s}.slurm'
 
     with open(script_name, 'w') as f:
         f.write(slurm_msg)
