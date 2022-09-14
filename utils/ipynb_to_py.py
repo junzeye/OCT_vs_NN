b18e4e0dbc1b4b261b4651f13439e33be6f1a2d7
Tony Ye <junzeye@gmail.com>
2022-09-14 13:19:45 -0400

    clobbered

--- utils/ipynb_to_py.py
+++ utils/ipynb_to_py.py
@@ -4,9 +4,9 @@ Code adapted from StackOverflow answer on converting .ipynb to .py
 
 import sys,json
 
-f = open('new_experiments.ipynb', 'r') #input.ipynb
+f = open('../new_experiments.ipynb', 'r') #input.ipynb
 j = json.load(f)
-of = open('experiment_instance.py', 'w') #output.py
+of = open('../experiment_instance.py', 'w') #output.py
 if j["nbformat"] >=4:
     for i,cell in enumerate(j["cells"]):
             for line in cell["source"]:
--- utils/submit_scripts.sh
+++ utils/submit_scripts.sh
@@ -1,7 +1,7 @@
 DIM=20
 SUB="dim_$DIM"
 
-for file in ./slurm_scripts/vary_width/*; do
+for file in ../slurm_scripts/vary_width/*; do
     if [[ "$file" == *"$SUB"* ]]; then
         sbatch $file
     fi
