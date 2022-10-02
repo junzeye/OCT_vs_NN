8a20524f891c07c7d9079bfe01093914bdd362e7
Tony Ye <junzeye@gmail.com>
2022-10-02 19:44:29 -0400

    automated oblique_experiment

--- oblique_experiment.py
+++ oblique_experiment.py
@@ -12,10 +12,10 @@ from scikit_obliquetree.segmentor import MeanSegmentor, TotalSegmentor, Gini
 
 start_time = time.time()
 
-MAX_TIME = 60 * 60 * 23.5 # safe exit time in seconds
+MAX_TIME = 60 * 60 * 64 # safe exit time in seconds
 max_depths = list(range(2,31,2)) # [2,4,6,8,,...,30]
 min_sample_leaves = [2,4,8,16,32]
-hidden_dim = 100
+hidden_dims = [20,40,60,80,100,120]
 
 random_states = [0,1,2,3]
 cv_random_states = {0:4, 1:5, 2:6, 3:7} # random_states when doing cross-validation
@@ -31,45 +31,44 @@ if path.isfile(CSV_FILEPATH):
 else:
     res_oblique = pd.DataFrame(columns=COLUMNS)
 
-# load data
-x, y = dataset.loadData(data, hidden_dim)
+for hidden_dim in hidden_dims:
+    # load data
+    x, y = dataset.loadData(data, hidden_dim)
 
-for d, s in itertools.product(max_depths, random_states):
-    # tree has not been trained, so train
-    # data splitting
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1-train_ratio, 
-                                    random_state=s)
-    scores_by_leaves = [0] * len(min_sample_leaves)
+    for d, s in itertools.product(max_depths, random_states):
+        # tree has not been trained, so train
+        # data splitting
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=1-train_ratio, 
+                                        random_state=s)
+        scores_by_leaves = [0] * len(min_sample_leaves)
 
-    tick = time.time()
+        tick = time.time()
 
-    for index, l in enumerate(min_sample_leaves):
-        sgmtr = TotalSegmentor()
-        cv = StratifiedKFold(shuffle = True, random_state = cv_random_states[s]) # default 5-fold cv
-        HHTree = HouseHolderCART(impurity = Gini(), segmentor = sgmtr, max_depth = d, min_samples = l)
-        scores = cross_val_score(HHTree, x_train, y_train, cv = cv)
-        scores_by_leaves[index] = sum(scores) / len(scores)
+        # use cross validation to select the optimal number of minimum samples per leaf
+        for index, l in enumerate(min_sample_leaves):
+            sgmtr = TotalSegmentor()
+            cv = StratifiedKFold(shuffle = True, random_state = cv_random_states[s]) # default 5-fold cv
+            HHTree = HouseHolderCART(impurity = Gini(), segmentor = sgmtr, max_depth = d, min_samples = l)
+            scores = cross_val_score(HHTree, x_train, y_train, cv = cv)
+            scores_by_leaves[index] = sum(scores) / len(scores)
+        
+        train_time = time.time() - tick
 
+        best_index = np.argmax(scores_by_leaves) # min_sample_leaf with the best validation accuracy
+    
+        HHTree = HouseHolderCART(impurity = Gini(), segmentor = sgmtr, max_depth = d, 
+                                    min_samples = min_sample_leaves[best_index])
+        HHTree.fit(x_train, y_train)
+        train_score = accuracy_score(y_train, HHTree.predict(x_train))
+        test_score = accuracy_score(y_test, HHTree.predict(x_test))
+        
+        row = {'NN_dim': hidden_dim, 'CART_depth': d, 'seed': s, 'min_samples_leaf': min_sample_leaves[best_index], 
+                'train_acc': train_score, 'test_acc': test_score, 'train_time': train_time}
+        res_oblique = res_oblique.append(pd.DataFrame.from_records([row]), ignore_index = True)
+        
         time_elapsed = time.time() - start_time
         if time_elapsed > MAX_TIME:
             break
-    
-    train_time = time.time() - tick
 
-    best_index = np.argmax(scores_by_leaves) # min_sample_leaf with the best validation accuracy
-    HHTree = HouseHolderCART(impurity = Gini(), segmentor = sgmtr, max_depth = d, 
-                                min_samples = min_sample_leaves[best_index])
-    HHTree.fit(x_train, y_train)
-    train_score = accuracy_score(y_train, HHTree.predict(x_train))
-    test_score = accuracy_score(y_test, HHTree.predict(x_test))
-    
-    row = {'NN_dim': hidden_dim, 'CART_depth': d, 'seed': s, 'min_samples_leaf': min_sample_leaves[best_index], 
-            'train_acc': train_score, 'test_acc': test_score, 'train_time': train_time}
-    res_oblique = res_oblique.append(pd.DataFrame.from_records([row]), ignore_index = True)
-    
-    time_elapsed = time.time() - start_time
-    if time_elapsed > MAX_TIME:
-        break
-
-res_oblique.sort_values(['NN_dim', 'CART_depth', 'test_acc'])
-res_oblique.to_csv('res/oblique.csv', index = False)
+    res_oblique.sort_values(['NN_dim', 'CART_depth', 'test_acc'])
+    res_oblique.to_csv('res/oblique.csv', index = False)
